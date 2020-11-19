c49a52e57b7bdb8e61d979d78c97f9c84c053975
KikoootheBB <franciscolamelas97@gmail.com>
2020-11-19 12:24:24 +0000

    Sinful Virtue V0.46
    Implementação da passagem do hub para os níveis.

---
+++ Sinful Virtue/Assets/Scripts/SceneManagement.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SceneManagement : MonoBehaviour
+{
+     void OnTriggerEnter2D(Collider2D other)
+    {
+        if (tag == "Door 3")
+        {
+            SceneManager.LoadScene("Wrath");
+        }
+        if (tag == "Door 7")
+        {
+            SceneManager.LoadScene("Sloth"); 
+        }
+        if (tag == "Hub Door")
+        {
+            SceneManager.LoadScene("Hub");
+        }
+    }
+}
