035e61797983a6a389026652c0943c9c662ddc06
gammabeta656 <72970401+gammabeta656@users.noreply.github.com>
2020-12-09 04:30:31 +0000

    Main Menu refeito com Start (direto para o Hub), Settings (nao funcional), Credits e Exit

---
+++ Sinful Virtue/Assets/Scripts/SettingsMenu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingsMenu : MonoBehaviour
+{
+    public void SetVolume(float volume)
+    {
+        Debug.Log(volume);
+    }
+
+    public void SetGamma(float gamma)
+    {
+        Debug.Log(gamma);
+    }
+}
