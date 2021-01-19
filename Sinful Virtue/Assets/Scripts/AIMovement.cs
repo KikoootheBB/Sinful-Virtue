d563e83f58bed4daffb6edc119e11e5568b7b05e
KikoootheBB <kikooothebb@gmail.com>
2021-01-19 21:01:33 +0000

    Sinful Virtue V0.65
    Alteração do AI para que os Pecados naveguem aleatoriamente pelo labirinto;

--- Sinful Virtue/Assets/Scripts/AIMovement.cs
+++ Sinful Virtue/Assets/Scripts/AIMovement.cs
@@ -5,18 +5,29 @@ using UnityEngine.AI;
 
 public class AIMovement : MonoBehaviour
 {
-    [SerializeField] Transform target;
+    [SerializeField] Transform[] points;
+    private int destPoint = 0;
     NavMeshAgent agent;
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
         agent.updateRotation = false;
         agent.updateUpAxis = false;
+        agent.autoBraking = false;
+        GotoNextPoint();
     }
 
     // Update is called once per frame
     void Update()
     {
-        agent.SetDestination(target.position);
+        if (!agent.pathPending && agent.remainingDistance < 0.5f)
+            GotoNextPoint();
+    }
+    void GotoNextPoint()
+    {
+        if (points.Length == 0)
+            return;
+        destPoint = Random.Range(0, points.Length);
+        agent.destination = points[destPoint].position;
     }
 }
